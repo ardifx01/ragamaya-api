995e97d340fc3e77ecc12ecd868a7554265ce64a
Rama Diaz <ramadiaz221@gmail.com>
2025-08-17 19:53:35 +0700

    feat: add auth middleware for storage apis

--- routers/storage_router.go
+++ routers/storage_router.go
@@ -2,18 +2,19 @@ package routers
 
 import (
 	"ragamaya-api/api/storages/controllers"
+	"ragamaya-api/pkg/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func StorageRoutes(r *gin.RouterGroup, compControllers controllers.CompControllers) {
 	storageGroup := r.Group("/storage")
+	storageGroup.Use(middleware.AuthMiddleware())
 	{
 		imageGroup := storageGroup.Group("/image")
 		{
 			imageGroup.POST("/upload", compControllers.Images)
 			imageGroup.POST("/upload/single", compControllers.Image)
-			imageGroup.GET("/getall", compControllers.FindAllImages)
 		}
 	}
 }
